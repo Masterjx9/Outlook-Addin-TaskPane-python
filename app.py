7d47c6a7298bc33bd784fc1269a84c7aafa03ec6
unknown <Jaton Justice>
2021-12-22 12:22:01 -0800

    modifying files

--- app.py
+++ app.py
@@ -1,6 +1,41 @@
 from flask import Flask
+from flask import render_template
+from flask.helpers import send_file
+
 app = Flask(__name__)
 
+# @app.route("/")
+# def hello():
+#     return "Hello, World!"
+
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    return render_template("index.html")
+
+@app.route("/taskpane.html")
+def taskpane():
+    return render_template("taskpane.html")
+
+@app.route("/commands.html")
+def commands():
+    return render_template("commands.html")
+
+@app.route("/assets/icon-16.png")
+def icon16():
+    return send_file("./static/assets/icon-16.png",mimetype='image/png')
+
+@app.route("/assets/icon-32.png")
+def icon32():
+    return send_file("./static/assets/icon-32.png",mimetype='image/png')
+
+@app.route("/assets/icon-64.png")
+def icon64():
+    return send_file("./static/assets/icon-64.png",mimetype='image/png')
+
+@app.route("/assets/icon-80.png")
+def icon128():
+    return send_file("./static/assets/icon-80.png",mimetype='image/png')
+
+@app.route("/assets/logo-filled.png")
+def iconlogofilled():
+    return send_file("./static/assets/logo-filled.png",mimetype='image/png')
